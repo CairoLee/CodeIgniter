619e08ec0fea3b724442304285752110cd2daa8b
CairoLee <cairoliyu@gmail.com>
2016-07-20 14:49:23 +0800

    改进 Doctrine 的连接配置文件，能够根据环境做出一些正确的逻辑适配

--- application/libraries/Doctrine.php
+++ application/libraries/Doctrine.php
@@ -19,6 +19,7 @@ use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
 use Doctrine\Common\Cache\ArrayCache;
 use Doctrine\DBAL\Logging\EchoSQLLogger;
+use Doctrine\ORM\Tools\Setup;
 
 class Doctrine
 {
@@ -33,36 +34,70 @@ class Doctrine
         }
         require $file_path;
 
-        $entitiesClassLoader = new ClassLoader('models', rtrim(APPPATH, "/"));
-        $entitiesClassLoader->register();
-        $proxiesClassLoader = new ClassLoader('Proxies', APPPATH . 'models/Proxies');
-        $proxiesClassLoader->register();
-
-        // Set up caches
-        $config = new Configuration;
-        $cache = new ArrayCache;
-        $config->setMetadataCacheImpl($cache);
-        $driverImpl = $config->newDefaultAnnotationDriver(array(APPPATH . 'models/Entities'));
-        $config->setMetadataDriverImpl($driverImpl);
-        $config->setQueryCacheImpl($cache);
-
-        $config->setQueryCacheImpl($cache);
-
-        // Proxy configuration
-        $config->setProxyDir(APPPATH . '/models/Proxies');
-        $config->setProxyNamespace('Proxies');
-
-        // Set up logger
-        $logger = new EchoSQLLogger;
-        $config->setSQLLogger($logger);
-
-        $config->setAutoGenerateProxyClasses(true);
-
         // Database connection information
         $connectionOptions = $this->convertDbConfig($db['default']);
 
-        // Create EntityManager
+        // With this configuration, your model files need to be in application/models/Entity
+        // e.g. Creating a new Entity\User loads the class from application/models/Entity/User.php
+        $models_namespace = 'Entities';
+        $models_path = APPPATH . 'models';
+        $proxies_dir = APPPATH . 'models/Proxies';
+        $metadata_paths = array(APPPATH . 'models/Entities');
+
+        // Set $dev_mode to TRUE to disable caching while you develop
+        $dev_mode = (ENVIRONMENT !== 'production');
+
+        // --------------------------------------------------------------------
+        // 设置 Redis 缓存
+        // --------------------------------------------------------------------
+        // 虽然在 Doctrine 内部已经实现了缓存逻辑，但是第一优先级并不是使用 Redis
+        // 所以这里我们单独设置一下，优先使用 Redis 缓存
+        //
+        // Doctrine 内部关于缓存的逻辑详见：
+        // vendor/doctrine/orm/lib/Doctrine/ORM/Tools/Setup.php 第 126 行
+        //
+        // 如果 $cache 为 null 且不是开发模式的话，就会依次使用：apc、xcache、memcache、redis 缓存
+        // 如果是在开发模式下的话，则使用 ArrayCache
+        // --------------------------------------------------------------------
+        $cache = NULL;
+        if ( ! $dev_mode)
+        {
+            if (extension_loaded('redis'))
+            {
+                // 如果在生产环境，且 redis 模块已被 php 加载，那么使用 redis 作为缓存驱动器
+                $redis = new \Redis();
+                $redis->connect('127.0.0.1', 6379);
+                $cache = new \Doctrine\Common\Cache\RedisCache();
+                $cache->setRedis($redis);
+            }
+            else
+            {
+                // 若 redis 模块没有被加载的话，也使用 ArrayCache 作为缓存驱动器
+                $cache = new ArrayCache();
+            }
+        }
+        else
+        {
+            // 如果是在开发模式下的话，则使用 ArrayCache 作为缓存驱动器
+            $cache = new ArrayCache();
+        }
+
+        // If you want to use a different metadata driver, change createAnnotationMetadataConfiguration
+        // to createXMLMetadataConfiguration or createYAMLMetadataConfiguration.
+        $config = Setup::createAnnotationMetadataConfiguration($metadata_paths, $dev_mode, $proxies_dir, $cache);
+
+        // --------------------------------------------------------------------
+        // Set up logger
+        // 若取消注释下面两行代码的话，在执行 vendor/bin/doctrine 的时候
+        // 会将相关的查询语句全部输出到终端（便于调试排错）
+        // --------------------------------------------------------------------
+        // $logger = new EchoSQLLogger;
+        // $config->setSQLLogger($logger);
+
         $this->em = EntityManager::create($connectionOptions, $config);
+
+        $loader = new ClassLoader($models_namespace, $models_path);
+        $loader->register();
     }
 
     /**
